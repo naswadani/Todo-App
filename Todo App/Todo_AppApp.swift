59e51f4e1476a6935d6f839cecc8d4fc4bdf63fd
Naswa Bryna Danikhansa <naswakhansa23@gmail.com>
2024-09-07 13:43:00 +0700

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by naswakhansa on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
